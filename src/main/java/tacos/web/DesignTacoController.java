47c8d74fa2d268ef3bee1ed5213470e2b0a89469
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-18 12:19:12 +0200

    add post request handler

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,8 @@ import tacos.domain.entities.Ingredient;
 import tacos.domain.entities.Ingredient.Type;
 import tacos.domain.entities.Taco;
 import tacos.domain.entities.TacoOrder;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -55,6 +58,21 @@ public class DesignTacoController {
     @GetMapping
     public String showDesignForm() {
         return "design";
+    }
+
+    @PostMapping
+    public String processTaco(
+            @Valid Taco taco, Errors errors,
+            @ModelAttribute TacoOrder tacoOrder) {
+
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+
+        return "redirect:/orders/current";
     }
 
     private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
