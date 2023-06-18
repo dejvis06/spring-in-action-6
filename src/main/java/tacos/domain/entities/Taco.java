ca9834ffbb9ba405f342bd12863df537bca2ebd2
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-18 10:30:54 +0200

    create Taco entity

---
+++ src/main/java/tacos/domain/entities/Taco.java
@@ -0,0 +1,11 @@
+package tacos.domain.entities;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<Ingredient> ingredients;
+}
