f542a524941cb8ca64e569a38fb97ea73bd43dff
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-20 10:14:12 +0200

    configure login form

--- src/main/java/tacos/web/WebConfig.java
+++ src/main/java/tacos/web/WebConfig.java
@@ -1,6 +1,7 @@
 package tacos.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import
         org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
