7395a662289e4fdf0be2c1970064020a8213cbd9
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-20 10:14:21 +0200

    configure login form and disable cors

--- src/main/java/tacos/web/security/SecurityConfig.java
+++ src/main/java/tacos/web/security/SecurityConfig.java
@@ -2,11 +2,27 @@ package tacos.web.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin()
+                .loginPage("/login")
+                .and()
+                .csrf()
+                .disable()
+                .build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
