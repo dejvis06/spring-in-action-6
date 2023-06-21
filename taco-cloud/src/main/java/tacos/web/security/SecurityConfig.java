a8a788535609db4bb29db35b03bed77286371f0b
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-21 17:38:33 +0200

    add resource-server configs

--- taco-cloud/src/main/java/tacos/web/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/web/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,22 +14,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests((authorize) -> authorize
+        http
+                .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/design", "/orders").hasRole("USER")
                         .requestMatchers(HttpMethod.POST, "/ingredients").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/ingredients/**").hasRole("ADMIN")
-                        .requestMatchers("/", "/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/design")
-                .and()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .csrf()
                 .disable()
                 .headers().frameOptions().disable().and()
                 .build();
+        return http.build();
     }
 
     @Bean
