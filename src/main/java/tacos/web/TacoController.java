df5fa8fb2e663072eda7d8bfa3194cd93765ee8e
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-21 10:20:25 +0200

    add Get by id

--- src/main/java/tacos/web/TacoController.java
+++ src/main/java/tacos/web/TacoController.java
@@ -2,11 +2,16 @@ package tacos.web;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tacos.domain.entities.Taco;
 import tacos.domain.repositories.TacoRepository;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/tacos",
@@ -23,5 +28,14 @@ public class TacoController {
         PageRequest page = PageRequest.of(
                 0, 12, Sort.by("createdOn").descending());
         return tacoRepository.findAll(page).getContent();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> optTaco = tacoRepository.findById(id);
+        if (optTaco.isPresent()) {
+            return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 }
