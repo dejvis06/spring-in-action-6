2fbcae0c21cf3ed712270829cf8b6d5ffa3e55ca
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-21 10:26:12 +0200

    add POST

--- src/main/java/tacos/web/TacoController.java
+++ src/main/java/tacos/web/TacoController.java
@@ -4,10 +4,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tacos.domain.entities.Taco;
 import tacos.domain.repositories.TacoRepository;
 
@@ -37,5 +34,11 @@ public class TacoController {
             return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
         }
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepository.save(taco);
     }
 }
