c21ec354219d56337f67387f405156c12ae8e23f
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-23 16:11:00 +0200

    add entities

---
+++ taco-cloud-restful-api/src/main/java/com/example/domain/entities/Taco.java
@@ -0,0 +1,35 @@
+package com.example.domain.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.List;
+
+@Data
+@Entity
+public class Taco {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
+    private String name;
+
+    @NotNull
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
+    @ManyToMany
+    private List<Ingredient> ingredients;
+
+    @CreationTimestamp
+    private Instant createdOn;
+
+    @UpdateTimestamp
+    private Instant lastUpdatedOn;
+}
