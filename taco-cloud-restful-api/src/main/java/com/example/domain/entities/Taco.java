881a5134d6068cf30d3baa67d14e52dcb3ea9c77
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-23 16:12:32 +0200

    remove validations

--- taco-cloud-restful-api/src/main/java/com/example/domain/entities/Taco.java
+++ taco-cloud-restful-api/src/main/java/com/example/domain/entities/Taco.java
@@ -1,8 +1,6 @@
 package com.example.domain.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -18,12 +16,8 @@ public class Taco {
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
 
-    @NotNull
-    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
-    @NotNull
-    @Size(min = 1, message = "You must choose at least 1 ingredient")
     @ManyToMany
     private List<Ingredient> ingredients;
 
