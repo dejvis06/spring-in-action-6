46b5d4b0bf607b6d66317b43cc3324558ee4e971
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-23 15:56:47 +0200

    remove resource server related code

--- taco-cloud-mvc/src/main/java/tacos/web/security/SecurityConfig.java
+++ taco-cloud-mvc/src/main/java/tacos/web/security/SecurityConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,8 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/design", "/orders").hasRole("USER")
@@ -23,21 +19,9 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.DELETE, "/ingredients/**").hasRole("ADMIN")
                         .requestMatchers("/", "/**").permitAll()
                         .anyRequest().authenticated()
-                )
-                .oauth2ResourceServer()
-                .jwt();
+                );
 
         return http.build();
     }
 
-    @Bean
-    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        grantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
-        grantedAuthoritiesConverter.setAuthorityPrefix("");
-
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
-        return jwtAuthenticationConverter;
-    }
 }
