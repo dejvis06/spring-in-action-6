955ee09a651c3633ffffc4d0cabac4809a9076c4
beqirajd <dejvis.beqiraj@linksmt.it>
2023-07-08 14:06:51 +0200

    switch to servlet app security configs

--- taco-cloud-mvc/src/main/java/tacos/web/configs/SecurityConfig.java
+++ taco-cloud-mvc/src/main/java/tacos/web/configs/SecurityConfig.java
@@ -2,27 +2,26 @@ package tacos.web.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebFluxSecurity
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    SecurityWebFilterChain configure(ServerHttpSecurity http) {
-        // @formatter:off
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
         http
-                .authorizeExchange((authorize) -> authorize
-                        .anyExchange().authenticated()
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
                 )
                 .oauth2Login(withDefaults())
                 .formLogin(withDefaults())
                 .oauth2Client(withDefaults())
                 .csrf().disable();
-        // @formatter:on
         return http.build();
     }
 }
