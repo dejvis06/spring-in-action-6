866f265d7edad76b9a6bd3efd715291056a5b3a4
beqirajd <dejvis.beqiraj@linksmt.it>
2023-07-08 17:09:43 +0200

    call order api

--- taco-cloud-mvc/src/main/java/tacos/web/OrderController.java
+++ taco-cloud-mvc/src/main/java/tacos/web/OrderController.java
@@ -3,6 +3,7 @@ package tacos.web;
 import jakarta.validation.Valid;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import tacos.domain.entities.TacoOrder;
 import tacos.domain.entities.User;
-import tacos.domain.repositories.OrderRepository;
 
 @Slf4j
 @Controller
@@ -22,11 +25,12 @@ import tacos.domain.repositories.OrderRepository;
 @SessionAttributes("tacoOrder")
 public class OrderController {
 
-    private OrderRepository orderRepository;
+    private final WebClient webClient;
 
-    public OrderController(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
+    public OrderController(WebClient webClient) {
+        this.webClient = webClient;
     }
+
 
     @GetMapping("/current")
     public String orderForm() {
@@ -41,7 +45,19 @@ public class OrderController {
         }
 
         order.setUser(user);
-        orderRepository.save(order);
+        Mono<TacoOrder> response = this.webClient.post()
+                .uri("/orders")
+                .body(Mono.just(order), TacoOrder.class)
+                .retrieve()
+                .bodyToMono(TacoOrder.class);
+
+        response.subscribe(tacoOrder -> {
+                    assert tacoOrder != null;
+                },
+                errors1 -> {
+                    assert errors1 != null;
+                }
+        );
 
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
