eef2e2859d7f570d66b91b92581b6d8983f1787f
beqirajd <dejvis.beqiraj@linksmt.it>
2023-07-08 17:09:14 +0200

    create controller

---
+++ taco-cloud-restful-api/src/main/java/com/example/web/OrderController.java
@@ -0,0 +1,27 @@
+package com.example.web;
+
+import com.example.domain.entities.TacoOrder;
+import com.example.domain.repositories.OrderRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/orders",
+        produces = "application/json")
+@Slf4j
+public class OrderController {
+
+    private final OrderRepository orderRepository;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TacoOrder postTaco(@RequestBody TacoOrder taco) {
+        log.info("Received order: {}", taco);
+        return orderRepository.save(taco);
+    }
+}
