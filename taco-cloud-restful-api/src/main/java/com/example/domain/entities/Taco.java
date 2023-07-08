7745040c93af1f13ebd7cb09b242fcf9736a5d56
beqirajd <dejvis.beqiraj@linksmt.it>
2023-07-08 17:09:07 +0200

    add validations

--- taco-cloud-restful-api/src/main/java/com/example/domain/entities/Taco.java
+++ taco-cloud-restful-api/src/main/java/com/example/domain/entities/Taco.java
@@ -1,12 +1,15 @@
 package com.example.domain.entities;
 
+import java.time.Instant;
+import java.util.List;
+
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.Instant;
-import java.util.List;
 
 @Data
 @Entity
@@ -16,8 +19,12 @@ public class Taco {
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
 
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
+    @NotNull
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
     @ManyToMany
     private List<Ingredient> ingredients;
 
