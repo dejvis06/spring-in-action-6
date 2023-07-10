378b5659e02d6eb19374408495091c647ae22adc
beqirajd <dejvis.beqiraj@linksmt.it>
2023-07-10 12:27:57 +0200

    change to long type

--- taco-cloud-restful-api/src/main/java/com/example/domain/entities/Ingredient.java
+++ taco-cloud-restful-api/src/main/java/com/example/domain/entities/Ingredient.java
@@ -1,18 +1,16 @@
 package com.example.domain.entities;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class Ingredient {
 
-    private final String id;
-    private final String name;
-    private final Type type;
+    @Id
+    private Long id;
+    private String name;
+    private Type type;
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
