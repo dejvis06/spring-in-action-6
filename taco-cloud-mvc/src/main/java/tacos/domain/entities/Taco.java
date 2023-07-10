cfbb54904f6f6d7d99976fe5bf49dae15df26b6b
beqirajd <dejvis.beqiraj@linksmt.it>
2023-07-10 12:24:22 +0200

    change to list to set

--- taco-cloud-mvc/src/main/java/tacos/domain/entities/Taco.java
+++ taco-cloud-mvc/src/main/java/tacos/domain/entities/Taco.java
@@ -1,22 +1,16 @@
 package tacos.domain.entities;
 
 import java.time.Instant;
-import java.util.List;
+import java.util.Set;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 @Data
-@Entity
 public class Taco {
 
-    @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
 
     @NotNull
@@ -25,12 +19,9 @@ public class Taco {
 
     @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    @ManyToMany
-    private List<Ingredient> ingredients;
+    private Set<Ingredient> ingredients;
 
-    @CreationTimestamp
     private Instant createdOn;
 
-    @UpdateTimestamp
     private Instant lastUpdatedOn;
 }
